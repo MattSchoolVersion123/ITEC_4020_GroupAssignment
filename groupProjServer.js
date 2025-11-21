c4ae23ef78119aeddf50e6907083078bdd4e55dc
Matthew_Llactahuamani <mattl72@my.yorku.ca>
2025-11-20 19:35:42 -0500

    added gets to practice using vs with nodejs

--- groupProjServer.js
+++ groupProjServer.js
@@ -1,1 +1,27 @@
+//Module packages imported
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const websocket = require('ws');
+
+//Middleware for module packages 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//Add welcome or sum like that for the home page then a please continue 
+app.get('/home', (req,res) => {
+    res.send('HELLO HOMEPAGE');
+});
+app.get('/about', (req,res) => {
+    res.send("HELLO ABOUT PAGE")
+});
+app.get('/education', (req,res) => {
+    res.send("HELLO EDUCATION")
+});
+app.get('/experience',(req,res) => {
+    res.send('HELLO EXPERIENCE')
+});
+app.get('/project', (req,res) => {
+    res.send('HELLO PROJECT PAGE')
+});
