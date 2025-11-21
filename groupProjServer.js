44751c712c1a5bd942121d4867102759271d9469
Matthew_Llactahuamani <mattl72@my.yorku.ca>
2025-11-20 20:27:03 -0500

    added middle ware and schema for chatgpt_eval

--- groupProjServer.js
+++ groupProjServer.js
@@ -9,19 +9,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+main().catch(err => console.log(err));
+async function main() { 
+    await mongoose.connect('mongodb://localhost:27017/chatgpt-evaluation-db')
+}
+
+const {Schema} = mongoose; 
+const chatgpt_eval_schema = new Schema ({
+    question: String,
+    expected_answer: String,
+    chatgpt_response: String,
+    domain: String
+});
+
+
+
 //Add welcome or sum like that for the home page then a please continue 
 app.get('/home', (req,res) => {
     res.send('HELLO HOMEPAGE');
 });
 app.get('/about', (req,res) => {
-    res.send("HELLO ABOUT PAGE")
+    res.send("HELLO ABOUT PAGE");
 });
 app.get('/education', (req,res) => {
-    res.send("HELLO EDUCATION")
+    res.send("HELLO EDUCATION");
 });
 app.get('/experience',(req,res) => {
-    res.send('HELLO EXPERIENCE')
+    res.send('HELLO EXPERIENCE');
 });
 app.get('/project', (req,res) => {
-    res.send('HELLO PROJECT PAGE')
+    res.send('HELLO PROJECT PAGE');
 });
+
