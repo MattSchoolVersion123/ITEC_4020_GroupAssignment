09a4f4b4f1a24a84ef191e31a4fe5f1c99931412
Matthew_Llactahuamani <mattl72@my.yorku.ca>
2025-11-20 21:35:00 -0500

    added /api/add route, validation and calculation

--- groupProjServer.js
+++ groupProjServer.js
@@ -12,7 +12,7 @@ app.use(express.json());
 main().catch(err => console.log(err));
 async function main() { 
     await mongoose.connect('mongodb://localhost:27017/chatgpt-evaluation-db')
-}
+};
 
 const {Schema} = mongoose; 
 const chatgpt_eval_schema = new Schema ({
@@ -22,7 +22,26 @@ const chatgpt_eval_schema = new Schema ({
     domain: String
 });
 
+app.use('/api/add', (req,res,next) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
 
+    if (!a || !b) {
+        return res.status(400).send('A and B are empty');
+    }
+    else if (isNaN(a) || isNaN(b))
+    {
+        return res.status(400).send('A or B is not a number');
+    }
+    req.a = a;
+    req.b = b;
+    next();
+});
+
+app.get('/api/add', (req,res) => {
+    const a = req.a + req.b;
+    res.json({result});
+});
 
 //Add welcome or sum like that for the home page then a please continue 
 app.get('/home', (req,res) => {
